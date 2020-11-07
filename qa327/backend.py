bbaef05262d32d93c27c3bb77ffe2389c0680f54
winterNebs <winterNebs@gmail.com>
2020-11-07 17:46:14 -0500

    use email validator

--- qa327/backend.py
+++ qa327/backend.py
@@ -1,15 +1,20 @@
 from qa327.models import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
-import re
+from email_validator import validate_email, EmailNotValidError
+
 """
 This file defines all backend logic that interacts with database and other services
 """
 def validate_email(email):
     """
     Validate that email follows RFC 5322
-    see stackoverflow for explanation https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression
     """
-    return re.fullmatch(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`'])`'])", email)
+    try:
+        validate_email(email)
+        return True
+    except EmailNotValidError as e :
+        return False
+
 
 def validate_password(password):
     """
