e1150761c66c8bcf39b68bec03efd14b22714226
winterNebs <winterNebs@gmail.com>
2020-11-16 13:35:18 -0500

    try test

--- qa327_test/backend/test_r1_post.py
+++ qa327_test/backend/test_r1_post.py
@@ -1,13 +1,13 @@
 import pytest
 from seleniumbase import BaseCase
-
+from qa327.models import db, User
 from qa327_test.conftest import base_url
 from unittest.mock import patch
 from werkzeug.security import generate_password_hash, check_password_hash
 
 test_user = User(
     email='test_frontend@test.com',
-    name='testuser'
+    name='testuser',
     password=generate_password_hash('test_frontendA1$')
 )
 
