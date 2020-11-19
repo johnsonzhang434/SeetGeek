755e1794f19ebdde48333a51dc52fb5706943be4
johnsonzhang434 <18jz1@queensu.ca>
2020-11-19 15:30:35 -0500

    Update frontend.py

--- qa327/frontend.py
+++ qa327/frontend.py
@@ -139,4 +139,4 @@ def profile(user):
 	# the login checking code all the time for other
 	# front-end portals
 	tickets = bn.get_all_tickets()
-  	return render_template('index.html', user=user, tickets=tickets, balance=user.balance)
+	return render_template('index.html', user=user, tickets=tickets, balance=user.balance)
